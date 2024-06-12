556d08a53b6ace2f982d016bbded3aec9d31835f
PhilV1 <werpub.phil@gmx.de>
2024-06-12 11:07:39 +0200

    chore: Update createProduct controller to upload and save product image

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -9,12 +9,49 @@ async function getProducts(req, res) {
   }
 }
 
-async function createProduct(req, res) {
-  const { name, description, image } = req.body
+import { cloudinary } from '../cloudinary/cloudinaryConfig.js'
+
+const createProduct = async (req, res) => {
+  // hole die Daten aus dem Request (Bild inklusive)
+  const { name, description, price, image } = req.body
+
+  // lade das Bild in Cloudinary hoch (cloudinary scheint eigene error handling zu haben)
+  const uploadedImage = await cloudinary.uploader.upload(
+    image,
+    {
+      upload_preset: 'cloudimage',
+      public_id: `${name}`,
+      allowed_formats: [
+        'jpg',
+        'png',
+        'jpeg',
+        'gif',
+        'svg',
+        'webp',
+        'jfif',
+        'ico',
+      ],
+    },
+    function (error, result) {
+      if (error) throw error
+    }
+  )
+
+  // du kannst das Bild in der Konsole ausgeben, um zu sehen, ob es funktioniert hat
+  console.log(uploadedImage)
+
+  // hole den `secure_url` aus dem `uploadedImage` Objekt.  Das ist der Pfad zum Bild in Cloudinary
+  const cloudImg = uploadedImage.secure_url
+
+  // hole den `public_id` aus dem `uploadedImage` Objekt.  Das brauchst du, um das Bild später zu löschen
+  const cloudImgPub = uploadedImage.public_id
+  // erstelle ein neues Produkt mit dem Bildpfad
   try {
     const product = new productModel({
       name,
       description,
+      image: cloudImg, // hier wird das Bildpfad gespeichert
+      imgpub: cloudImgPub, // hier wird das public_id gespeichert
     })
     await product.save()
     res.status(201).json({ message: 'Product erfolgreich gespeichert' })
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import 'dotenv/config'
 const app = express()
 const PORT = process.env.API_PORT || 5000
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '30mb' }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use('/products', productRouter)
 
 app.get('/', (req, res) => {
