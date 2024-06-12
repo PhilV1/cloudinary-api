1b40c96472dceba9f6d97156136b6e29f6a74293
PhilV1 <werpub.phil@gmx.de>
2024-06-12 11:15:43 +0200

    chore: Add notFoundMiddleware and errorHandlerMiddleware to server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './database/connectDB.js'
 import productRouter from './routes/productRoutes.js'
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
+import notFoundMiddleware from './middleware/notFoundMiddleware.js'
 import 'dotenv/config'
 
 const app = express()
@@ -10,6 +12,8 @@ app.use(cors())
 app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use('/products', productRouter)
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 app.get('/', (req, res) => {
   res.send('API is running...')
